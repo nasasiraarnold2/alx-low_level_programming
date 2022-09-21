f320cf4526bf0340e40b74217df21224863812b4
nasasiraarnold2 <nasasiraarnold20@gmail.com>
2022-09-21 10:53:15 -0700

    Task 5. Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,10 +8,11 @@
 
 char *string_toupper(char *s)
 {
-char str[] = "Look up!\n";
-char *ptr;
-ptr = string_toupper(str);
-putchar("%s", ptr);
-putchar("%s", str);
-return (0);
+int count = 0;
+while (*(s + count) != '\0')
+if ((*(s + count) >= 97) && (*(s + count) <= 122))
+*(s + count) = *(s + count) - 32;
+count++;
+} 
+return (s);
 }
